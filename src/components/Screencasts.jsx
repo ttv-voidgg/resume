260a8c860f2600c2ac08134fa86ce69bd3cd81cf
eejay89 <juancarlos.deborja@gmail.com>
2022-07-12 19:45:11 +0800

    fixed certifications

--- src/components/Screencasts.jsx
+++ src/components/Screencasts.jsx
@@ -3,39 +3,30 @@ import Image from 'next/future/image'
 import { Container } from '@/components/Container'
 import { SectionHeading } from '@/components/SectionHeading'
 import duotoneImage from '@/images/screencasts/duotone.svg'
-import gridsImage from '@/images/screencasts/grids.svg'
-import setupImage from '@/images/screencasts/setup.svg'
-import strokesImage from '@/images/screencasts/strokes.svg'
+import gridsImage from '@/images/screencasts/ielts.png'
+import setupImage from '@/images/screencasts/google-cert.png'
+import strokesImage from '@/images/screencasts/wes.png'
 import { GridPattern } from '@/components/GridPattern'
 
 const videos = [
   {
-    title: 'Getting started with Figma',
+    title: 'Google PMP Certification',
     description:
       'Get familiar with the Figma UI, the different tools it offers, and the most important features.',
     image: setupImage,
-    runtime: { minutes: 16, seconds: 54 },
+    url: ''
   },
   {
-    title: 'Setting up your artboard',
+    title: 'IELTS Certification',
     description:
       'Learn how to create a new artboard and configure your grid and rulers for designing icons.',
     image: gridsImage,
-    runtime: { minutes: 9, seconds: 12 },
   },
   {
-    title: 'Designing your first icon',
+    title: 'WES Assessment',
     description:
       'Using basic shapes and boolean operations, learn how to design your own notification icon from scratch.',
     image: strokesImage,
-    runtime: { minutes: 23, seconds: 25 },
-  },
-  {
-    title: 'Advanced design techniques',
-    description:
-      'Learn the techniques you need to know to adapt your original icon to a modern duotone style.',
-    image: duotoneImage,
-    runtime: { minutes: 28, seconds: 44 },
   },
 ]
 
@@ -79,36 +70,25 @@ export function Screencasts() {
           pro in just over an hour.
         </p>
       </Container>
-      <Container size="lg" className="mt-16">
+      <Container size="md" className="mt-16">
         <ol
           role="list"
-          className="grid grid-cols-1 gap-y-10 gap-x-8 [counter-reset:video] sm:grid-cols-2 lg:grid-cols-4"
+          className="grid grid-cols-1 text-center gap-y-10 gap-x-8 md:grid-cols-3"
         >
           {videos.map((video) => (
-            <li key={video.title} className="[counter-increment:video]">
+            <li key={video.title} className="">
               <div
-                className="relative flex h-44 items-center justify-center rounded-2xl px-6 shadow-lg"
+                className="relative  items-center justify-center rounded-2xl p-1 shadow-lg max-w-xs mx-auto"
                 style={{
                   backgroundImage:
                     'conic-gradient(from -49.8deg at 50% 50%, #7331FF 0deg, #00A3FF 59.07deg, #4E51FF 185.61deg, #39DBFF 284.23deg, #B84FF1 329.41deg, #7331FF 360deg)',
                 }}
               >
-                <div className="flex overflow-hidden rounded shadow-sm">
-                  <Image src={video.image} alt="" unoptimized />
-                </div>
-                <div className="absolute bottom-2 left-2 flex items-center rounded-lg bg-black/30 px-1.5 py-0.5 text-sm text-white [@supports(backdrop-filter:blur(0))]:bg-white/10 [@supports(backdrop-filter:blur(0))]:backdrop-blur">
-                  <PlayIcon className="h-4 w-4 fill-current stroke-current" />
-                  <time
-                    dateTime={`${video.runtime.minutes}m ${video.runtime.seconds}s`}
-                    className="ml-2"
-                  >
-                    {`${video.runtime.minutes}:${video.runtime.seconds
-                      .toString()
-                      .padStart(2, '0')}`}
-                  </time>
+                <div className="overflow-hidden rounded-xl shadow-sm">
+                  <Image src={video.image} alt="" className="w-full" unoptimized />
                 </div>
               </div>
-              <h3 className="mt-8 text-base font-medium tracking-tight text-slate-900 before:mb-2 before:block before:font-mono before:text-sm before:text-slate-500 before:content-[counter(video,decimal-leading-zero)]">
+              <h3 className="mt-8 text-base font-medium tracking-tight text-slate-900 before:mb-2 before:block before:font-mono before:text-sm before:text-slate-500">
                 {video.title}
               </h3>
               <p className="mt-2 text-sm text-slate-600">{video.description}</p>
