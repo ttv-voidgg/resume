3b44fbdf188b3178036630f16940062e245ba8fb
Eejay <juancarlos.deborja@gmail.com>
2022-12-18 00:36:45 -0600

    December

--- src/components/FreeChapters.jsx
+++ src/components/FreeChapters.jsx
@@ -29,12 +29,12 @@ export function FreeChapters() {
             </p>
           </div>
           <div className="lg:pl-16 z-10 lg:mt-14">
-            <a href="tel:+639277770744">
+            <a href="tel:+12048692714">
               <div className="mb-2 max-w-xl bg-white text-slate-700 text-lg sm:text-xl rounded-full px-3 py-2">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2 mr-2 inline-block" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
               </svg>
-              +6392 - 7777 - 0744
+              +1 (204) 869 - 2714
               </div>
             </a>
             <a href="mailto:juancarlos.deborja@gmail.com">
