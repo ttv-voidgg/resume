0f96f5e600d80a7b1d911938d5545be5b59ed7b8
ttv-voidgg <juancarlos.deborja@gmail.com>
2024-05-03 19:14:17 -0500

    Update FreeChapters.jsx
    number update

--- src/components/FreeChapters.jsx
+++ src/components/FreeChapters.jsx
@@ -34,7 +34,7 @@ export function FreeChapters() {
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2 mr-2 inline-block" viewBox="0 0 20 20" fill="currentColor">
                 <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
               </svg>
-              +1 (204) 869 - 2714
+              +1 (431) 275 - 7015
               </div>
             </a>
             <a href="mailto:juancarlos.deborja@gmail.com">
