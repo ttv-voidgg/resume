9e93d5834571eef376c75de4b576d3ef98dbe669
Eejay <juancarlos.deborja@gmail.com>
2024-08-13 15:16:24 -0500

    Link Changes

--- src/components/FreeChapters.jsx
+++ src/components/FreeChapters.jsx
@@ -54,7 +54,7 @@ export function FreeChapters() {
               Book a meeting
               </div>
             </a>             
-            <a href="https://www.linkedin.com/in/eejay-de-borja-80696651/" target="_blank" rel="noreferrer">
+            <a href="https://www.linkedin.com/in/juan-carlos-eejay-de-borja-80696651/" target="_blank" rel="noreferrer">
               <div className="mb-2 max-w-xl bg-white text-slate-700 text-lg sm:text-xl rounded-full px-3 py-2">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 ml-2 mr-2 inline-block" width="24" height="24" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"/></svg>
               Linkedin Profile
