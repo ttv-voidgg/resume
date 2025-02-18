9478535f030024ae1bc0a39a9bc01bf38be635b7
Eejay <juancarlos.deborja@gmail.com>
2025-02-18 15:47:02 -0600

    Just a bit of edits

--- src/components/Screencasts.jsx
+++ src/components/Screencasts.jsx
@@ -20,6 +20,16 @@ const videos = [
     status: 'Course 2 of 6',
   },
   {
+    title: 'Google PMP Certification',
+    description:
+      'Credential ID: W6S8XJ5J5K3S',
+    image: google,
+    url: 'https://www.coursera.org/account/accomplishments/certificate/W6S8XJ5J5K3S',
+    target: '_blank',
+    referrer: 'noreferrer',
+    status: 'Course 2 of 6',
+  },  
+  {
     title: 'IELTS Certification',
     description:
       'Report #: 22PH004208DEJ001G',
